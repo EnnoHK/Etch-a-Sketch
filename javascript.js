da6afe08772696652c9c1e771b43f0f3a34920d9
EnnoHK <zokikame@hotmail.com>
2022-07-26 12:37:57 -0700

    Implemented grid and box creation with userinput

---
+++ javascript.js
@@ -0,0 +1,56 @@
+var i = 0;
+var userInput = 16;
+const element = document.getElementById("myBtn");
+
+element.addEventListener("click", getSize);
+
+function getSize() {
+    userInput = prompt("Input the size of the grid: ");
+
+    while (i < (userInput * userInput)) {
+        var div = document.createElement("div");
+        div.style.width = "";
+        div.style.height = "";
+        // div.style.background = "white";
+        div.id = "box"; //maybe id is the problem | change to class without destroying the code 
+
+        document.getElementById("grid-container").appendChild(div);
+        // console.log(i);
+        i++;
+
+        createGridSize();
+        activatePainting();
+    }
+
+}
+
+function createGridSize() {
+    let boxSize = 960 / userInput;
+    console.log(boxSize);
+    document.getElementById("grid-container").style.gridTemplateColumns = `repeat(${userInput}, ${boxSize}px)`;
+    document.getElementById("grid-container").style.gridTemplateRows = `repeat(${userInput}, ${boxSize})`;
+    document.getElementById("grid-container").style.width = "960px";
+    document.getElementById("grid-container").style.height = "960px";
+
+}
+
+function activatePainting() {
+    var boxes = document.getElementById("box");
+    boxes.addEventListener('mouseover', () => {
+        // boxes.style.backgroundColor = 'black';
+        document.getElementById("box").style.backgroundColor = "black";
+    });
+}
+
+/*
+box.addEventListener('mouseover', function handleMouseOver() {
+  box.style.color = 'red';
+});
+
+
+
+function changeColor() {
+
+
+
+*/
