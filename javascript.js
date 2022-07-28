f4df09f529bca09578255d3713a4dd5c120fd56b
EnnoHK <zokikame@hotmail.com>
2022-07-28 10:28:07 -0700

    Functioning Etch-a-Sketch created

--- javascript.js
+++ javascript.js
@@ -6,27 +6,43 @@ element.addEventListener("click", getSize);
 
 function getSize() {
     userInput = prompt("Input the size of the grid: ");
+    // const boxes = document.querySelectorAll(".box");
+    // boxes.forEach(box => {
+    //     box.remove();
+    // });
 
     while (i < (userInput * userInput)) {
-        var div = document.createElement("div");
+        let div = document.createElement("div");
         div.style.width = "";
         div.style.height = "";
         // div.style.background = "white";
-        div.id = "box"; //maybe id is the problem | change to class without destroying the code 
+        // div.id = "box"; //maybe id is the problem | change to class without destroying the code 
 
-        document.getElementById("grid-container").appendChild(div);
+        // document.getElementById("grid-container").appendChild(div);
         // console.log(i);
+        div.classList.add("box");
+        document.getElementById("grid-container").appendChild(div);
         i++;
 
         createGridSize();
-        activatePainting();
+        // activatePainting();
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'black';
+            // document.getElementById("box").style.backgroundColor = "black";
+        });
+        div.addEventListener('click', () => {
+            div.style.backgroundColor = 'white';
+            document.getElementById("box").style.backgroundColor = "white";
+        });
+
+
     }
 
 }
 
 function createGridSize() {
     let boxSize = 960 / userInput;
-    console.log(boxSize);
+    // console.log(boxSize);
     document.getElementById("grid-container").style.gridTemplateColumns = `repeat(${userInput}, ${boxSize}px)`;
     document.getElementById("grid-container").style.gridTemplateRows = `repeat(${userInput}, ${boxSize})`;
     document.getElementById("grid-container").style.width = "960px";
@@ -34,13 +50,17 @@ function createGridSize() {
 
 }
 
-function activatePainting() {
-    var boxes = document.getElementById("box");
-    boxes.addEventListener('mouseover', () => {
-        // boxes.style.backgroundColor = 'black';
-        document.getElementById("box").style.backgroundColor = "black";
-    });
-}
+// function activatePainting() {
+//     var boxes = document.getElementsByClassName("box");
+//     boxes.addEventListener('mouseover', () => {
+//         boxes.style.backgroundColor = 'black';
+//         // document.getElementById("box").style.backgroundColor = "black";
+//     });
+//     boxes.addEventListener('click', () => {
+//         boxes.style.backgroundColor = 'white';
+//         document.getElementById("box").style.backgroundColor = "white";
+//     });
+// }
 
 /*
 box.addEventListener('mouseover', function handleMouseOver() {
